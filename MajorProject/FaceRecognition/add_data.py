b9de820e72d791260ec633022b4bbddff36ecb7a
Rizwan Un Nissa <rizwanunnissa@gmail.com>
2024-12-20 15:53:11 +0530

    Adding Major Project

---
+++ MajorProject/FaceRecognition/add_data.py
@@ -0,0 +1,33 @@
+import face_recognition as fr
+import pickle 
+import cv2
+from model import load_data
+
+path = './dataset.pkl'
+
+def get_data(name, image):
+
+	data = load_data()
+	
+	image = fr.load_image_file(image)
+
+	idx = len(data)
+
+	encoding = fr.face_encodings(image)[0]
+
+	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+	data[idx]['image'] = image
+	data[idx]['name'] = name
+	data[idx]['Id'] = idx - 1
+	data[idx]['encoding'] = encoding
+
+	with open(path , "wb") as f:
+		pickle.dump(data,f)
+
+	return True
+
+print(len(load_data()))
+
+#get_data("Roger Federer", "C:/Users/hp/OneDrive/Desktop/Riz/opencv_facerecognition/dataset/Original Images/Original Images/Roger Federer/Roger Federer_2.jpg")
+
